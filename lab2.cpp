8b56526a009a278023da5e633e8be598b4b2e2bf
Amperage-mi <63056849+Amperage-mi@users.noreply.github.com>
2023-11-30 12:57:20 +0300

    Create lab2.cpp

---
+++ lab2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+
+// picture 2
+
+using namespace std;
+
+// определение знчения функции "у" в зависимотси от аргумента "х", по заданному графику.
+float line (int x) 
+{
+    float y=0;
+    if(x >= -10 && x < -8)
+    {
+        y = -3;
+    }
+    if (x >= -8 && x < -3)
+    { 
+        y = (x+3)*(0.6);
+    }
+    if (x >= -3 && x < 3)
+    {
+        y = sqrt (9 - x * x);
+    }
+    if (x >= 3 && x <= 5)
+    {
+        y = x - 3;
+    } 
+    if (x > 5 && x <=8)
+    {
+        y = 3;
+    }
+    return y;
+}
+
+// цикл для вывода значений функции, определяемой графиком.
+float main()
+{
+    cout << "START" << endl;
+    for ( int x = - 10; x <= 8; x++ )
+    {
+        cout << "x = " << x << "   y = " << line (x) << endl;
+
+    }
+    cout << "END";
+}
